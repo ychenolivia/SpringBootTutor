b2dcf792713fdac1351628a86b080ee6e2197e86
ychenolivia <45774566+ychenolivia@users.noreply.github.com>
2020-01-14 20:26:12 -0500

    Update CourseController.java

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.TwoSum;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +16,13 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class CourseController {
+    @Autowired
+    TwoSum twoSum;
 
-    @GetMapping(path = "/", produces = "application/json")
-    public HttpEntity findAllCourses(){
-       // List<CourseDto> allCourses = courseService.findAllCourses();
+    @GetMapping(path = "/look-up/{inputString}", produces = "application/json")
+    public HttpEntity FindIndex(@PathVariable("inputString") String inputString) {
+        int[] res = twoSum.getIndex(inputString);
 
-        return new ResponseEntity<>("OK",HttpStatus.OK);
+        return new ResponseEntity(res, HttpStatus.OK);
     }
 }
